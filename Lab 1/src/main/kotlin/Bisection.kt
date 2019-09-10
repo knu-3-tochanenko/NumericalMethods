6621d3576aff213a0e9ae64862896e0e49244a45
Vladislav <itochanenko@gmail.com>
2019-09-10 23:01:32 +0300

    Added calculations count and execution time

--- Lab 1/src/main/kotlin/Bisection.kt
+++ Lab 1/src/main/kotlin/Bisection.kt
@@ -1,20 +1,36 @@
 import kotlin.math.absoluteValue
 import kotlin.math.pow
+import kotlin.system.measureTimeMillis
 
 class Bisection(
     var function: (x: Float) -> Float,
     var numberOfDigits: Int
 ) {
-    private val precision: Float = 0.1.pow(numberOfDigits.toDouble()).toFloat()
-    fun calculate(start: Float, end: Float, numberOfSteps: Int): Float {
-        val mid = (start + end) / 2
+    private val precision: Float = 0.1f.pow(numberOfDigits)
 
-        println("Operation #$numberOfSteps: Calculating between $start and $end. Function value is ${function(mid)}")
-        val functionValue = function(mid)
-        return when {
-            functionValue.absoluteValue <= precision -> mid
-            functionValue > 0 -> calculate(start, mid, numberOfSteps + 1)
-            else -> calculate(mid, end, numberOfSteps + 1)
+    fun calculate(startP: Float, endP: Float): CalculationResult {
+        var mid: Float = (endP + startP) / 2
+        var start = startP
+        var end = endP
+        var calculations = 0
+        val milliseconds = measureTimeMillis {
+            while ((end - start).absoluteValue > precision) {
+                calculations++
+                mid = (end + start) / 2
+                if (function(mid).absoluteValue <= precision)
+                    break
+                if (function(mid) > 0)
+                    end = mid
+                else
+                    start = mid
+            }
         }
+        return CalculationResult(mid, calculations, milliseconds)
     }
 }
+
+data class CalculationResult(
+    val result: Float,
+    val calculations: Int,
+    val time: Long
+)
--- Lab 1/src/main/kotlin/Main.kt
+++ Lab 1/src/main/kotlin/Main.kt
@@ -9,8 +9,9 @@ class Main {
         fun function(x: Float): Float = x.pow(3) + 8
 
         fun testBisection() {
-            val bisection = Bisection(::function, 8)
-            print("Result of bisection is ${bisection.calculate(-10F, 3F, 0)}")
+            val bisection = Bisection(::function, 12)
+            val res = bisection.calculate(-10F, 3F)
+            print("Result of bisection is ${res.result}.\nIn ${res.calculations} calculations in ${res.time} time")
         }
     }
 }
