51a801037c853213bb850a108ef52853f766499f
Vladislav <itochanenko@gmail.com>
2019-09-19 23:15:05 +0300

    Added Jacobi method, perfected output

--- Lab 2/src/main/kotlin/Gauss.kt
+++ Lab 2/src/main/kotlin/Gauss.kt
@@ -1,7 +1,9 @@
 object Gauss {
     fun solve(matrix: Matrix): Array<Double> {
         val res: Array<Double> = Array(matrix.elements) {0.0}
+        var iterations = 0
         for (i in 0 until matrix.elements - 1) {
+            iterations++
             for (k in i + 1 until matrix.elements) {
                 val t = (matrix[k, i] / matrix[i, i])
                 for (j in 0..matrix.elements)
@@ -9,6 +11,7 @@ object Gauss {
             }
         }
         for (i in matrix.elements - 1 downTo 0) {
+            iterations++
             res[i] = matrix[i, matrix.elements]
             for (j in i + 1 until matrix.elements) {
                 if (j != i)
@@ -16,6 +19,7 @@ object Gauss {
             }
             res[i] /= matrix[i, i]
         }
+        print(" : ($iterations)$ANSI_RESET\n")
         return res
     }
 }
--- Lab 2/src/main/kotlin/Main.kt
+++ Lab 2/src/main/kotlin/Main.kt
@@ -3,26 +3,28 @@ class Main {
         @JvmStatic fun main(args:Array<String>) {
             println("<3")
 
-            println("\n$ANSI_GREEN----- Gauss. Random -----$ANSI_RESET")
             val random = Matrix(3)
             random.generateRandom()
-            random.regenerateWithResult(1.0, 2.0, 3.0)
-            Gauss.solve(random).forEach { print("$ANSI_CYAN%.3f\t$ANSI_RESET".format(it)) }
-            print(random)
+            testMatrix(random, "RANDOM")
 
-            println("\n$ANSI_GREEN----- Gauss. Ok -----$ANSI_RESET")
             val ok = Matrix(3)
             ok.generateOk()
-            ok.regenerateWithResult(1.0, 2.0, 3.0)
-            Gauss.solve(ok).forEach { print("$ANSI_CYAN%.3f\t$ANSI_RESET".format(it)) }
-            print(ok)
+            testMatrix(ok, "OK")
 
-            println("\n$ANSI_GREEN----- Gauss. NotOk -----$ANSI_RESET")
             val notok = Matrix(3)
             notok.generateOk()
-            notok.regenerateWithResult(1.0, 2.0, 3.0)
-            Gauss.solve(notok).forEach { print("$ANSI_CYAN%.3f\t$ANSI_RESET".format(it)) }
-            print(notok)
+            testMatrix(notok, "NOT OK")
+
+        }
+
+        fun testMatrix(matrix: Matrix, name: String) {
+            print("\n\n$ANSI_GREEN------- $name -------$ANSI_RESET")
+            matrix.regenerateWithResult(1.0, 2.0, 3.0)
+            print(matrix)
+            print("\n${ANSI_GREEN}Gauss")
+            Gauss.solve(matrix).forEach { print("$ANSI_CYAN%.3f\t$ANSI_RESET".format(it)) }
+            print("\n${ANSI_GREEN}Jacobi")
+            Jacobi.solve(matrix, 0.0001).forEach { print("$ANSI_CYAN%.3f\t$ANSI_RESET".format(it)) }
         }
     }
 }
