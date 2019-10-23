0fdc7d1c545484dd75d69425ef0680aedfcd4bdb
Vladislav <itochanenko@gmail.com>
2019-10-23 11:07:44 +0300

    Vector b is now a val

--- Lab 4/src/main/kotlin/Main.kt
+++ Lab 4/src/main/kotlin/Main.kt
@@ -10,14 +10,15 @@ class Main {
                 1.0, 5.0, 0.0,
                 5.0, 10.0, 30.0
             )
+            val b = doubleArrayOf(10.5, 16.0, 38.0)
             print("${ANSI_YELLOW}Calculated manually (on paper) :" +
                     "${ANSI_GREEN}\n1.000\t3.000\t0.100${ANSI_RESET}")
             print("\n\n${ANSI_YELLOW}Gauss method :${ANSI_RESET}")
-            print(Gauss.solve(matrix, doubleArrayOf(10.5, 16.0, 38.0)))
+            print(Gauss.solve(matrix, b))
             print("\n\n${ANSI_YELLOW}Jacobi method :${ANSI_RESET}")
-            print(Jacobi.solve(matrix, doubleArrayOf(10.5, 16.0, 38.0)))
+            print(Jacobi.solve(matrix, b))
             print("\n\n${ANSI_YELLOW}Seidel method :${ANSI_RESET}")
-            print(Seidel.solve(matrix, doubleArrayOf(10.5, 16.0, 38.0)))
+            print(Seidel.solve(matrix, b))
         }
     }
 }
