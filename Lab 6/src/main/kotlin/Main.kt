2db0102d38bae4ea290b126069fc2436dcc4ed4d
Vladislav <itochanenko@gmail.com>
2019-10-29 12:20:59 +0200

    Initialized project for Lab 6

---
+++ Lab 6/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+class Main {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Love u to the end of the kus`mus` and back <3")
+        }
+    }
+}
